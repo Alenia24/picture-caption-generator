020dcd1bdaa7ecdc7612818b106212594bb4a4e8
Alenia Allen <112526474+Alenia24@users.noreply.github.com>
2024-12-28 16:56:23 -0500

    Improve user experience

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ function displayGift(response) {
     delay: 0.1,
     cursor: "",
   });
+
+  let searchInput = document.querySelector("#search");
+  searchInput.value = "";
+
 }
 function generateGift(event) {
   event.preventDefault();
@@ -12,11 +16,18 @@ function generateGift(event) {
   let searchInput = document.querySelector("#search");
 
   let apiKey = "fb2da4606e22fot4bcd3216b032ab697";
-  let context = "You are an AI Gift Assistant that tells short gift ideas. Provide the answer in a HTML format. Example: <p><strong>occassion<strong></p> <p>This is a gift idea</p>. Make sure to follow the user instructions. Sign the gift with gift giving wishes and '<strong>AI Gift Generator<strong>' on separate lines with no padding and on separate lines";
-
-  let prompt = `User Instructions: Generate a gift based on ${searchInput.value}`;
+  let context =
+    "You are an AI Gift Assistant that gives short gift ideas. Provide the answer in a HTML format. Example: <p><strong>occassion<strong></p> <ul><li><p>This is a gift idea</p></li> <li><p>This is a gift idea</p></li> <li><p>This is a gift idea</p><li></ul>. Make sure to follow the user instructions. Add a gift giving celebration wishes. Sign '<strong>AI Gift Generator<strong><br />''.Donot make the wishes have a strong property.";
+  let prompt = `User Instructions: Generate three gifts based on ${searchInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  
+
+
+  let giftElement = document.querySelector(".gift");
+  giftElement.innerHTML = `
+  <div class="loading">
+    <img src="assets/loading.png" alt="">
+  </div>`;
+
   axios.get(apiUrl).then(displayGift);
 }
 
